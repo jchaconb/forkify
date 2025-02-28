0c0876c07b87f779842152944a870ab1cab2bc28
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-02-28 13:39:36 -0600

    Updates

--- index.e37f48ea.js
+++ index.e37f48ea.js
@@ -1946,6 +1946,8 @@ parcelHelpers.defineInteropFlag(exports);
 var _iconsSvg = require("../../img/icons.svg");
 var _iconsSvgDefault = parcelHelpers.interopDefault(_iconsSvg);
 var _fractional = require("fractional");
+var _fractionalDefault = parcelHelpers.interopDefault(_fractional);
+console.log((0, _fractionalDefault.default));
 class RecipeView {
     #parentElement = document.querySelector('.recipe');
     #data;
@@ -2049,7 +2051,7 @@ class RecipeView {
         <svg class="recipe__icon">
           <use href="${0, _iconsSvgDefault.default}#icon-check"></use>
         </svg>
-        <div class="recipe__quantity">${ing.quantity ? new (0, _fractional.Fraction)(ing.quantity).toString() : ''}</div>
+        <div class="recipe__quantity">${ing.quantity ? new (0, _fractionalDefault.default)(ing.quantity).toString() : ''}</div>
         <div class="recipe__description">
           <span class="recipe__unit">${ing.unit}</span>
           ${ing.description}
