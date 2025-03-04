cee7f09f8a8a0328fe393e617b2e19daba97e7df
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-03-04 16:53:30 -0600

    Section 18.320: Wrapping Up: Final Considerations

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -2320,7 +2320,16 @@ var _iconsSvg = require("url:../../img/icons.svg");
 var _iconsSvgDefault = parcelHelpers.interopDefault(_iconsSvg);
 class View {
     _data;
-    render(data, render = true) {
+    // https://jsdoc.app/
+    /**
+   * Render the received object to the DOM
+   * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render=false
+   * @this {Object} View instance
+   * @author Jose Chacon
+   * @todo Finish implementation
+   */ render(data, render = true) {
         if (!data || Array.isArray(data) && data.length === 0) return this.renderError();
         this._data = data;
         const markup = this._generateMarkup();
